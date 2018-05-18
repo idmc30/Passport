88f8e5747e7949d172f07d05e9c57ca22bb57457
idmc <id.morales10n@gmail.com>
2018-05-17 22:44:26 -0500

    gerando ruta de la api - api/post

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('posts', function (){
+	return App\Post::all();
+})->middleware('auth:api');
