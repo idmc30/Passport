6dd027800b5d6de8d549ae84ea18805dba6b7415
idmc <id.morales10n@gmail.com>
2018-05-17 21:22:07 -0500

    registrando el service provider

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
+        Barryvdh\Cors\AppServiceProvider::class,
 
         /*
          * Package Service Providers...
